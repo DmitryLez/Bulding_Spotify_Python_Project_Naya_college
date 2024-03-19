dd7724a7def0d55deda4f70b0889bf18eb89178b
DmitryLez <dmitryone1@gmail.com>
2024-03-19 19:03:39 +0200

    songs_normalize.csv data file was added.

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("first step")
+import pandas as pd
+pd.set_option('display.max_columns', 24)
+df = pd.read_csv('spotify-2023.csv', encoding='latin1') #loading the data
+columns_to_keep = ['track_name', 'artist(s)_name', 'released_year','in_spotify_playlists','streams']
+
+print(df.head(1))
+print(df.shape)
