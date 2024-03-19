de61d6a23cbe57abf57923ef554d2771965a9e48
DmitryLez <dmitryone1@gmail.com>
2024-03-19 19:07:36 +0200

    update to csv file.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 pd.set_option('display.max_columns', 24)
-df = pd.read_csv('spotify-2023.csv', encoding='latin1') #loading the data
+df = pd.read_csv('songs_normalize.csv', encoding='latin1') #loading the data
 columns_to_keep = ['track_name', 'artist(s)_name', 'released_year','in_spotify_playlists','streams']
 
 print(df.head(1))
